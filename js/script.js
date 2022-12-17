7882fd98235c469fbf1e4365b80c122a9cccdc86
Alexander Kharmansky <davilex_x@vk.com>
2022-12-18 03:06:58 +0600

    feat: add skills section, start projects section

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const bg = document.querySelector(".main__bg");
+
+window.addEventListener("scroll", () => {
+  var scroll =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+  bg.style.opacity = Math.max(0, Math.min(1, -scroll / 400 + 2));
+});
